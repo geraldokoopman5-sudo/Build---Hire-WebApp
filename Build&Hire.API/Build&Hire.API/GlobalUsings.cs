d1b57dbb5c0bda5d12a76b18bb6213bbbd0d03f8
unknown <geraldokoopman5@gmail.com>
2026-07-27 08:03:50 +0200

    service files

---
+++ Build&Hire.API/Build&Hire.API/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using BuildAndHire.Domain.Models;
+global using BuildAndHire.Domain.Enums;
+global using BuildAndHire.Domain.ValueObjects;
+global using Microsoft.EntityFrameworkCore;
+global using BuildAndHire.Application.Interfaces.Repositories;
+global using BuildAndHire.Application.Interfaces.Repository;
+global using Microsoft.EntityFrameworkCore.Design;
+global using BuildAndHire.Infrastructure.Data;
